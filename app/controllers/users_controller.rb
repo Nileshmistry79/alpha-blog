84676269f6a086a8498511cd2f9b204b83aa8c91
Nilesh Ruby Rails dummy projects <nilesh.mistry79@gmail.com>
2016-10-01 10:27:51 +0000

    Add signed upon signed in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -36,12 +36,13 @@ class UsersController < ApplicationController
   
   
   def create
-      #debugger
+      
       @user=User.new(user_param)
       
       if @user.save
+        session[:user_id]=@user.id
         flash[:success]="Welcome to Alpha blog #{@user.username}"
-        redirect_to articles_path
+        redirect_to user_path(@user)
       else
         render 'new'
       end
