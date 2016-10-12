164ed25c6e4e6392c2b39ef12138088c71b97ed2
Nilesh Ruby Rails dummy projects <nilesh.mistry79@gmail.com>
2016-10-12 10:46:29 +0000

    only admin can add new categories

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -4,6 +4,7 @@ class CategoriesControllerTest < ActionController::TestCase
   
   def setup
     @category = Category.create(name: "sport")
+    @user = User.create(username: "joohn", email: "a@test.com", password: "password", admin: true)
   end  
 
   test "Should get category index"  do
@@ -14,6 +15,7 @@ class CategoriesControllerTest < ActionController::TestCase
   end
   
   test "should get name " do
+       session[:user_id]=@user.id
        get :new
       assert_response :success
   end
@@ -22,5 +24,15 @@ class CategoriesControllerTest < ActionController::TestCase
      get(:show, {'id' => @category.id})
       assert_response :success
   end  
+  
+    test  "should redirect to page when admin is not logged in" do
+      assert_no_difference 'Category.count' do
+        post :create, category: { name: "sports"}
+      end 
+      assert_redirected_to categories_path
+  end  
+
+
+  
   
 end
