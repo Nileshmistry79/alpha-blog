4b45f33fccd94f21e4ec5dc56c9063d1ae146e37
Nilesh Ruby Rails dummy projects <nilesh.mistry79@gmail.com>
2016-10-14 05:44:26 +0000

    add edit category feature

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -27,6 +27,23 @@ class CategoriesController < ApplicationController
     
   end
   
+  
+  def edit
+      @category= Category.find(params[:id])
+  end
+  
+  def update
+    @category= Category.find(params[:id])
+    if @category.update(category_parmas)
+      flash[:success]="Category update"
+      redirect_to category_path(@category)
+    else
+      render edit
+    end   
+  end  
+  
+  
+  
   private 
     def category_parmas
       params.require(:category).permit(:name)
